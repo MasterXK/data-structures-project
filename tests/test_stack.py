480858ba2df95cf6a93e5639e109bb64ae0879b4
MasterXK <34077712+MasterXK@users.noreply.github.com>
2023-12-18 05:30:06 +0000

    Added test_pop

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,17 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+from src.stack import Stack
+
+
+def test_pop():
+    stack = Stack()
+    stack.push('data1')
+    stack.push('data2')
+    data = stack.pop()
+
+    assert data == 'data2'
+
+    data = stack.pop()
+
+    assert stack.top is None
+
+    assert data == 'data1'
